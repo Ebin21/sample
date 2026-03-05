97c6570748de5171a73893b198ac56b5a372ce9f
Ebin21 <ebin.alex@outlook.com>
2026-03-05 13:34:10 +0530

    update commit sample

---
+++ salary.py
@@ -0,0 +1,5 @@
+newsalary = 0
+salary = input("enter new salary : ")
+raise_percent = input("salary raise rate(%) : ")
+newsalary = int(salary)+(int(salary)*int(raise_percent)/100)
+print("increased salary :",newsalary)
