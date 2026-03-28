7d865c65f719dd2f8bec7758658d6aaaf22c6fff
Ebin21 <ebin.alex@outlook.com>
2026-03-28 14:02:44 +0530

    update commit sample

--- avg.py
+++ avg.py
@@ -1,6 +1,11 @@
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+num1 = input('Enter number of inputs: ')
+num = []
+sum = 0
+for i in range(int(num1)):
+    n = input(f'Enter number {i+1}: ')
+    num.append(int(n))
+    sum += int(n)
 
-average =(int(num1) + int(num2))/2
+average = sum / int(num1) if int(num1) > 0 else 0
 
 print('average:{0} '.format(average))
