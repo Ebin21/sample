69250e37380b24d7410dbaed2074f096fe37108b
Ebin21 <ebin.alex@outlook.com>
2026-08-28 23:43:02 +0530

    Push all commit Sample

--- unit.py
+++ unit.py
@@ -1,7 +1,11 @@
 total=0
+count=0 
 while True:
   num = float(input("enter a number: "))
+
   if num==0:
     break
   total+=num
+  count+=1
 print("The sum of the numbers is: ",total)
+print("The average of the numbers is: ",total/count if count > 0 else 0)
