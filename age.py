7b4034f5abe948880289e638df1f2bef438008e3
Ebin21 <ebin.alex@outlook.com>
2026-09-02 15:32:28 +0530

    Push all commit Sample

---
+++ age.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age: "))
+#age = 18
+
+#expression that can be true or false
+
+if age > 18:
+    print("you can enter" )
+elif age == 18:
+    print("go see Pink Floyd")
+else:
+    print("go see Meat Loaf" )
+    
+print("move on")
