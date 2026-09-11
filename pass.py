49ece4a8595d3f0def5541015542d52cde62b793
Ebin21 <ebin.alex@outlook.com>
2026-09-11 06:10:29 +0530

    Push all commit Sample

--- pass.py
+++ pass.py
@@ -10,11 +10,15 @@ def is_valid_password(password):
     return False
 # Accept input from the user as comma-separated passwords
 passwords = input("Enter passwords separated by commas: ").split(',')
+print("Entered passwords:", passwords)
 # Initialize a list to store valid passwords
 valid_passwords = []
 # Iterate through the passwords and check their validity
 for psw in passwords:
     if is_valid_password(psw):
         valid_passwords.append(psw)
+    else:
+        print(f"Invalid password: {psw}")
+        break
 # Print the valid passwords 
-print(','.join(valid_passwords))
+print(f'Valid passwords: ' + ', '.join(valid_passwords))
